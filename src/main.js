fa8e7bce02680e2422baa76690899b3db4b0a535
Marcin Górski <m.gorski@empressia.pl>
2019-11-27 23:04:54 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/tailwind.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(vuex);
+Vue.use(router);
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
